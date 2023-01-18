44b837b7fa5d963f4e7b386dc60a8771cae0fcc8
Lahrasab Razman <s3neo36@gmail.com>
2023-01-18 19:36:04 +0500

    clients

---
+++ clients.php
@@ -0,0 +1,159 @@
+<?php
+require("./partials/header-1.php");
+?>
+        <!-- header close -->
+
+        <!-- subheader begin -->
+        <section id="subheader" class="page-client no-bottom" data-stellar-background-ratio="0.5">
+            <div class="inner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1>Clients &amp; Partners</h1>
+                            <div class="crumb">
+                                <div>
+                                    <ul>
+                                        <li><a href="index-2.html">Home</a></li>
+										<li><a href="about.html">About</a></li>
+                                        <li>Clients</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- subheader close -->
+
+        <div class="clearfix"></div>
+
+        <!-- content begin -->
+        <div id="content" class="no-padding">
+
+            <!-- section begin -->
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>About Our Company</h2>
+							<p class="lead">BGT is a fast-growing logistics support provider across multiple verticals including Oil & Gas/ petroleum,
+distribution.
+</p>
+							<div class="divider-deco"><span></span></div>
+							<p>BGT was established in 2005 and provides integrated supply chain solutions across UAE. With our extensive local footprint and strong expertise in supply chain logistics, we offer unparalleled levels
+of customer satisfaction.</p>
+							
+							<div class="divider-double"></div>
+							
+							<h2>They Says</h2>
+							<div class="divider-deco"><span></span></div>
+							<div id="testi-carousel-2" class="testi-slider wow fadeIn" data-wow-delay="0s" data-wow-duration="1s">
+                                <div class="item">
+                                    <blockquote>
+                                        Gocargo people professional and courteous attitude, expert knowledge and patience handling our specific issues are very important to our customer
+
+                                    </blockquote>
+                                    <div class="arrow-down"></div>
+                                    <div class="testi-by">
+                                        <img src="img/about/testimonials/testimonial-1.jpg" class="img-circle" alt="">
+                                        <span class="name"><strong>Jane Salima</strong>, General Manager<br>
+                                            Nexus Motors</span>
+                                    </div>
+                                </div>
+
+                                <div class="item">
+                                    <blockquote>
+                                        Your representatives have very helpful attitude and their attention to detail help us to move our work smoothly without any delay...good job guys
+
+                                    </blockquote>
+                                    <div class="arrow-down"></div>
+                                    <div class="testi-by">
+                                        <img src="img/about/testimonials/testimonial-2.jpg" class="img-circle" alt="">
+                                        <span class="name"><strong>Dave Smith</strong>, Sales Manager<br>
+                                            Jepara Furniture</span>
+                                    </div>
+                                </div>
+
+                                <div class="item">
+                                    <blockquote>
+                                        I'm really impressed by the professionalism that the Gocargo relocation. Now we stay with our new studio at Metro city thanks Gocargo
+
+                                    </blockquote>
+                                    <div class="arrow-down"></div>
+                                    <div class="testi-by">
+                                        <img src="img/about/testimonials/testimonial-3.jpg" class="img-circle" alt="">
+                                        <span class="name"><strong>Mike Martien</strong>, Web Designer<br>
+                                            Bamskies Studio</span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+					<div class="col-md-6">
+						<div class="row">
+							<div class="col-md-4 mb30">
+								<img src="img/clients/adnoc.png" class="img-responsive" alt="">
+							</div>
+							
+							<div class="col-md-4 mb30">
+								<img src="img/clients/bgc.png" class="img-responsive" alt="">
+							</div>
+							
+							<div class="col-md-4 mb30">
+								<img src="img/clients/cgc.png" class="img-responsive" alt="">
+							</div>
+							
+							<div class="col-md-4 mb30">
+								<img src="img/clients/npc.webp" class="img-responsive" alt="">
+							</div>
+							
+							<div class="col-md-4 mb30">
+								<img src="img/clients/modulargc.webp" class="img-responsive" alt="">
+							</div>
+							
+							<div class="col-md-4 mb30">
+								<img src="img/clients/trojan.png" class="img-responsive" alt="">
+							</div>
+							
+							<div class="col-md-4 mb30">
+								<img src="img/clients/resized/adnoc.png" class="img-responsive" alt="">
+							</div>
+						</div>
+					</div>
+                        
+                    </div>
+
+
+                </div>
+            </section>
+
+            <!-- section begin -->
+            <section id="section-cta-2" class="cta light-text" data-stellar-background-ratio="0.5" class="light-text">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="padding30 overlaydark80 wow fadeIn">
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <h2 class="mb0 mt10">Contact us now to get quote for all your global shipping needs</h2>
+                                    </div>
+                                    <div class="col-md-2 text-right">
+                                        <a href="#" class="btn btn-custom">Contact Us</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- section close -->
+
+        </div>
+        <!-- content close -->
+
+        <!-- footer begin -->
+        <?php
+require("./partials/footer.php");
+?>
