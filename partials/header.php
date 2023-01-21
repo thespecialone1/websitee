2093166c3cdd0ac48194261c02b493131f3d1171
Lahrasab Razman <s3neo36@gmail.com>
2023-01-21 16:30:25 +0500

    some changes

--- partials/header.php
+++ partials/header.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <title>Bal Ayara Gen. Transport Company Website</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Bal Ayara General Transport company located in Abu Dhabi">
+    <meta name="author" content="Bal Ayara">
 
     <!-- LOAD CSS FILES -->
     <link href="css/main.css" rel="stylesheet" type="text/css">
