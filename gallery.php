b0383f0620469fe1e48b80207818befd1ce1e06a
Lahrasab Razman <s3neo36@gmail.com>
2023-01-24 18:36:22 +0500

    added pics

--- gallery.php
+++ gallery.php
@@ -32,6 +32,20 @@ require("./partials/header-1.php");
 
             <div id="gallery" class="gallery full-gallery ex-gallery zoom-gallery col-3">
 
+
+            <div class="item photography mobile">
+                    <div class="picframe">
+                        <a href="img/gallery/resized/19-1.jpg">
+                            <span class="overlay"></span>
+
+                            <span class="pf_text">
+                                <span class="project-name">Crew Memeber</span>
+                                <span class="small-border"></span>
+                            </span>
+                            <img src="img/gallery/resized/19.jpg" alt="" />
+                        </a>
+                    </div>
+                </div>
                 <!-- gallery item -->
                 <div class="item illustration">
                     <div class="picframe">
@@ -39,7 +53,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Military Logistic</span>
+                                <span class="project-name">Oil Tanker</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/1.jpg" alt="" />
@@ -55,7 +69,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Our Heavy Truck</span>
+                                <span class="project-name">Oil Tanker</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/4.jpg" alt="" />
@@ -63,7 +77,21 @@ require("./partials/header-1.php");
                     </div>
                 </div>
                 <!-- close gallery item -->
+<!-- close gallery item -->
+<div class="item photography mobile">
+                    <div class="picframe">
+                        <a href="img/gallery/resized/20-1.jpg">
+                            <span class="overlay"></span>
 
+                            <span class="pf_text">
+                                <span class="project-name">Our Heavy Tankers</span>
+                                <span class="small-border"></span>
+                            </span>
+                            <img src="img/gallery/resized/20.jpg" alt="" />
+                        </a>
+                    </div>
+                </div>
+                <!-- close gallery item -->
                 <!-- gallery item -->
                 <div class="item website mobile">
                     <div class="picframe">
@@ -71,7 +99,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Ocean Freight Cargo</span>
+                                <span class="project-name">Truck</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/6.jpg" alt="" />
@@ -79,15 +107,27 @@ require("./partials/header-1.php");
                     </div>
                 </div>
                 <!-- close gallery item -->
-
-                <!-- gallery item -->
-                <div class="item illustration photography">
+                <div class="item photography mobile">
                     <div class="picframe">
-                        <a href="img/gallery/resized/9.jpg">
+                        <a href="img/gallery/resized/17-1.jpg">
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Heavy Duty Forklifts</span>
+                                <span class="project-name">Heavy Trucks</span>
+                                <span class="small-border"></span>
+                            </span>
+                            <img src="img/gallery/resized/17.jpg" alt="" />
+                        </a>
+                    </div>
+                </div>
+                <!-- gallery item -->
+                <div class="item illustration photography">
+                    <div class="picframe">
+                        <a href="img/gallery/resized/8.jpg">
+                            <span class="overlay"></span>
+
+                            <span class="pf_text">
+                                <span class="project-name">Light Trucks</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/8.jpg" alt="" />
@@ -95,7 +135,19 @@ require("./partials/header-1.php");
                     </div>
                 </div>
                 <!-- close gallery item -->
+                <div class="item photography mobile">
+                    <div class="picframe">
+                        <a href="img/gallery/resized/16-1.jpg">
+                            <span class="overlay"></span>
 
+                            <span class="pf_text">
+                                <span class="project-name">Heavy Trucks with Highly Experienced Drivers</span>
+                                <span class="small-border"></span>
+                            </span>
+                            <img src="img/gallery/resized/16.jpg" alt="" />
+                        </a>
+                    </div>
+                </div>
                 <!-- gallery item -->
                 <div class="item website photography mobile">
                     <div class="picframe">
@@ -103,7 +155,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Logistics Aircraft</span>
+                                <span class="project-name">Waste Collection Trucks</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/9.jpg" alt="" />
@@ -119,7 +171,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Logistics Leadership Training</span>
+                                <span class="project-name">LIght Trucks</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/10.jpg" alt="" />
@@ -127,7 +179,19 @@ require("./partials/header-1.php");
                     </div>
                 </div>
                 <!-- close gallery item -->
+                <div class="item photography mobile">
+                    <div class="picframe">
+                        <a href="img/gallery/resized/18-1.png">
+                            <span class="overlay"></span>
 
+                            <span class="pf_text">
+                                <span class="project-name">Heavy Truckse</span>
+                                <span class="small-border"></span>
+                            </span>
+                            <img src="img/gallery/resized/18.png" alt="" />
+                        </a>
+                    </div>
+                </div>
                 <!-- gallery item -->
                 <div class="item illustration photography">
                     <div class="picframe">
@@ -135,7 +199,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Wood Container</span>
+                                <span class="project-name">Oil Container</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/11.jpg" alt="" />
@@ -151,7 +215,7 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Railway Transport Logistics</span>
+                                <span class="project-name">Light Truck</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/13.jpg" alt="" />
@@ -167,13 +231,24 @@ require("./partials/header-1.php");
                             <span class="overlay"></span>
 
                             <span class="pf_text">
-                                <span class="project-name">Cargo Airline</span>
+                                <span class="project-name">Heavy Trucks</span>
                                 <span class="small-border"></span>
                             </span>
                             <img src="img/gallery/resized/15.jpg" alt="" />
                         </a>
                     </div>
                 </div>
+
+                <!-- ------------------------------------------------------------------- -->
+                <!-- close gallery item -->
+         
+                
+                <!-- close gallery item -->
+               
+                
+                
+                <!-- close gallery item -->
+                
                 <!-- close gallery item -->
 
 
